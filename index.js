53b709f1ad051e10e58420a151b96cbde9058c58
Adam <151994381+bilaljaved03@users.noreply.github.com>
2024-01-05 07:12:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ import  express  from "express";
 const app = express();
 const port = 9000;
 
+app.get('/getName',(req,res)=>{
+
+ res.json({message:"Hello Adam freeman"});
+
+});
+
 app.use('/',(req,res)=>{
 
     res.json({message:"Hello from Express Hosted app"})
