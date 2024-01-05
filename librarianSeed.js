5464eafcb10174ca8c295f5c27b4cf54c869745f
Adam <151994381+bilaljaved03@users.noreply.github.com>
2024-01-05 09:19:00 -0800

    Add files via upload

---
+++ librarianSeed.js
@@ -0,0 +1,227 @@
+const librarian= [
+	{
+		"id" : 1,
+		"name" : "Madame Victoria Francois",
+		"email" : "victoria.francois@example.com",
+		"password" : "1003",
+		"image" : "https://randomuser.me/api/portraits/women/38.jpg",
+		"date" : null
+	},
+	{
+		"id" : 2,
+		"name" : "Mr Ludvig Gilberg",
+		"email" : "ludvig.gilberg@example.com",
+		"password" : "hardcore",
+		"image" : "https://randomuser.me/api/portraits/men/0.jpg",
+		"date" : null
+	},
+	{
+		"id" : 3,
+		"name" : "Mr Juan Méndez",
+		"email" : "juan.mendez@example.com",
+		"password" : "rugger",
+		"image" : "https://randomuser.me/api/portraits/men/44.jpg",
+		"date" : null
+	},
+	{
+		"id" : 4,
+		"name" : "Mr Malthe Kristensen",
+		"email" : "malthe.kristensen@example.com",
+		"password" : "252525",
+		"image" : "https://randomuser.me/api/portraits/men/5.jpg",
+		"date" : null
+	},
+	{
+		"id" : 5,
+		"name" : "Ms Aida Velásquez",
+		"email" : "aida.velasquez@example.com",
+		"password" : "radar",
+		"image" : "https://randomuser.me/api/portraits/women/40.jpg",
+		"date" : null
+	},
+	{
+		"id" : 6,
+		"name" : "Ms غزل سهيلي راد",
+		"email" : "gzl.shylyrd@example.com",
+		"password" : "felix1",
+		"image" : "https://randomuser.me/api/portraits/women/26.jpg",
+		"date" : null
+	},
+	{
+		"id" : 7,
+		"name" : "Mr Ronald Torres",
+		"email" : "ronald.torres@example.com",
+		"password" : "action",
+		"image" : "https://randomuser.me/api/portraits/men/29.jpg",
+		"date" : null
+	},
+	{
+		"id" : 8,
+		"name" : "Mr Divyesh Belligatti",
+		"email" : "divyesh.belligatti@example.com",
+		"password" : "fishhead",
+		"image" : "https://randomuser.me/api/portraits/men/5.jpg",
+		"date" : null
+	},
+	{
+		"id" : 9,
+		"name" : "Mr Oliver Gill",
+		"email" : "oliver.gill@example.com",
+		"password" : "3ip76k2",
+		"image" : "https://randomuser.me/api/portraits/men/24.jpg",
+		"date" : null
+	},
+	{
+		"id" : 10,
+		"name" : "Mr Liam Faure",
+		"email" : "liam.faure@example.com",
+		"password" : "jumbo",
+		"image" : "https://randomuser.me/api/portraits/men/44.jpg",
+		"date" : null
+	},
+	{
+		"id" : 11,
+		"name" : "Mr Aapo Tuomi",
+		"email" : "aapo.tuomi@example.com",
+		"password" : "lemons",
+		"image" : "https://randomuser.me/api/portraits/men/79.jpg",
+		"date" : null
+	},
+	{
+		"id" : 12,
+		"name" : "Miss Leonora Martins",
+		"email" : "leonora.martins@example.com",
+		"password" : "1235",
+		"image" : "https://randomuser.me/api/portraits/women/34.jpg",
+		"date" : null
+	},
+	{
+		"id" : 13,
+		"name" : "Miss Liana Nerdal",
+		"email" : "liana.nerdal@example.com",
+		"password" : "jesse",
+		"image" : "https://randomuser.me/api/portraits/women/60.jpg",
+		"date" : null
+	},
+	{
+		"id" : 14,
+		"name" : "Mr Charlie Flores",
+		"email" : "charlie.flores@example.com",
+		"password" : "closeup",
+		"image" : "https://randomuser.me/api/portraits/men/79.jpg",
+		"date" : null
+	},
+	{
+		"id" : 15,
+		"name" : "Mr Seth Lambert",
+		"email" : "seth.lambert@example.com",
+		"password" : "toshiba",
+		"image" : "https://randomuser.me/api/portraits/men/89.jpg",
+		"date" : null
+	},
+	{
+		"id" : 16,
+		"name" : "Miss Niva Bhoja",
+		"email" : "niva.bhoja@example.com",
+		"password" : "hardone",
+		"image" : "https://randomuser.me/api/portraits/women/80.jpg",
+		"date" : null
+	},
+	{
+		"id" : 17,
+		"name" : "Miss Annie Bell",
+		"email" : "annie.bell@example.com",
+		"password" : "quick",
+		"image" : "https://randomuser.me/api/portraits/women/18.jpg",
+		"date" : null
+	},
+	{
+		"id" : 18,
+		"name" : "Ms Elizabeth Jean-Baptiste",
+		"email" : "elizabeth.jean-baptiste@example.com",
+		"password" : "bian",
+		"image" : "https://randomuser.me/api/portraits/women/79.jpg",
+		"date" : null
+	},
+	{
+		"id" : 19,
+		"name" : "Miss Mabel Vergunst",
+		"email" : "mabel.vergunst@example.com",
+		"password" : "nitram",
+		"image" : "https://randomuser.me/api/portraits/women/13.jpg",
+		"date" : null
+	},
+	{
+		"id" : 20,
+		"name" : "Mr Özkan Dalkıran",
+		"email" : "ozkan.dalkiran@example.com",
+		"password" : "calimero",
+		"image" : "https://randomuser.me/api/portraits/men/62.jpg",
+		"date" : null
+	},
+	{
+		"id" : 21,
+		"name" : "Mr Nihal Chiplunkar",
+		"email" : "nihal.chiplunkar@example.com",
+		"password" : "peterson",
+		"image" : "https://randomuser.me/api/portraits/men/2.jpg",
+		"date" : null
+	},
+	{
+		"id" : 22,
+		"name" : "Mrs Shubha Shroff",
+		"email" : "shubha.shroff@example.com",
+		"password" : "fiesta",
+		"image" : "https://randomuser.me/api/portraits/women/88.jpg",
+		"date" : null
+	},
+	{
+		"id" : 23,
+		"name" : "Mrs Stella Williams",
+		"email" : "stella.williams@example.com",
+		"password" : "jester",
+		"image" : "https://randomuser.me/api/portraits/women/55.jpg",
+		"date" : null
+	},
+	{
+		"id" : 24,
+		"name" : "Mr Yilmaz Joseph",
+		"email" : "yilmaz.joseph@example.com",
+		"password" : "sharpe",
+		"image" : "https://randomuser.me/api/portraits/men/58.jpg",
+		"date" : null
+	},
+	{
+		"id" : 25,
+		"name" : "Mr Eugenio Luevano",
+		"email" : "eugenio.luevano@example.com",
+		"password" : "mohawk",
+		"image" : "https://randomuser.me/api/portraits/men/71.jpg",
+		"date" : null
+	},
+	{
+		"id" : 26,
+		"name" : "Miss Eva Johnson",
+		"email" : "eva.johnson@example.com",
+		"password" : "babylon5",
+		"image" : "https://randomuser.me/api/portraits/women/26.jpg",
+		"date" : null
+	},
+	{
+		"id" : 27,
+		"name" : "Mr Klaus-Peter Schmuck",
+		"email" : "klaus-peter.schmuck@example.com",
+		"password" : "picture",
+		"image" : "https://randomuser.me/api/portraits/men/30.jpg",
+		"date" : null
+	},
+	{
+		"id" : 28,
+		"name" : "Mrs Julie Warren",
+		"email" : "julie.warren@example.com",
+		"password" : "orchid",
+		"image" : "https://randomuser.me/api/portraits/women/5.jpg",
+		"date" : null
+	}
+]
+module.exports = librarian;
